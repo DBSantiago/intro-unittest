8aae836f515ce8b941aecdbdd9f53acd1d727d1f
DBSantiago <deblasissantiago@gmail.com>
2022-06-03 16:56:20 -0300

    ADD: assertTrue and assertFalse tests

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,2 +1,17 @@
+from typing import List
+
+from product import Product
+
+
 class ShoppingCart:
-    pass
+    def __init__(self) -> None:
+        self.__products: List[Product] = list()
+
+    def empty(self) -> bool:
+        return len(self.__products) == 0
+
+    def has_products(self) -> bool:
+        return not self.empty()
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,6 +1,7 @@
 import unittest
 
 from product import Product
+from shopping_cart import ShoppingCart
 
 
 class TestShoppingCart(unittest.TestCase):
@@ -16,6 +17,10 @@ class TestShoppingCart(unittest.TestCase):
     def setUp(self) -> None:
         self.smartphone = Product("IPhone", 500.00)
         print("Method setUp is executed before each test.")
+
+        self.shopping_cart_1 = ShoppingCart()
+        self.shopping_cart_2 = ShoppingCart()
+        self.shopping_cart_2.add_product(self.smartphone)
 
     def tearDown(self) -> None:
         print("Method tearDown is executed before each test.")
@@ -34,6 +39,13 @@ class TestShoppingCart(unittest.TestCase):
 
     def test_product_price(self) -> None:
         self.assertEqual(self.smartphone.price, 500.00)
+
+    def test_shopping_cart_empty(self) -> None:
+        self.assertTrue(self.shopping_cart_1.empty(), "Shopping cart is not empty.")
+
+    def test_shopping_cart_has_product(self) -> None:
+        self.assertTrue(self.shopping_cart_2.has_products())
+        self.assertFalse(self.shopping_cart_2.empty())
 
 
 if __name__ == "__main__":
